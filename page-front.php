49b68d6bd67a2a491347c9fbd2c6d472e7587feb
Emily Schleiner <cordial.emily@gmail.com>
2017-09-25 11:22:01 -0700

    wp posts with exerpt and thumbnail to front-page template

--- page-front.php
+++ page-front.php
@@ -60,23 +60,36 @@ Learn more about how we make decisions about which issues to prioritize here.</h
           </div>
         </div>
         <div class="container">
-          <div class="row">
-            <div class="col-xs-12 jumbotron">
-              <?php
-    query_posts( array('posts_per_page'=>5, 'category_name'=>'Front Page') );
-    while ( have_posts() ) : the_post();
-?>
-    <h2><a href="<?php the_permalink(' ') ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-    <?php if ( has_post_thumbnail() ): // check for the featured image ?>
-    <a href="<?php the_permalink(' ') ?>" title="<?php the_title(); ?>" class="opacity"><?php the_post_thumbnail('thumbnail', array('class' => 'thumbnail')); ?></a> <hr> <!--echo the featured image-->
-<?php
-    endif;
-    the_excerpt(); // echo the excerpt
-    endwhile;
-    wp_reset_query(); // resets main query
-?>
 
-            </div>
+              <!--// Define our WP Query Parameters -->
+              <?php $the_query = new WP_Query( array('posts_per_page'=>5, 'category_name'=>'Front Page') ); ?>
+
+              <!--// Start our WP Query -->
+              <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+              <div class="row">
+                <div class="col-sm-2">
+                  <a href="<?php the_permalink(' ') ?>" title="<?php the_title(); ?>" class="opacity"><?php the_post_thumbnail('thumbnail', array('class' => 'thumbnail')); ?></a>
+                </div>
+
+
+                <div class="col-sm-10">
+                  <!--// Display the Post Title with Hyperlink -->
+                  <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+
+                  <!--// Display the Post Excerpt -->
+                  <p><?php the_excerpt(__('(more…)')); ?></p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-12"> <hr> </div>
+              </div>
+              <!--// Repeat the process and reset once it hits the limit -->
+              <?php
+              endwhile;
+              wp_reset_postdata();
+              ?>
+
+
           </div>
         </div>
 
