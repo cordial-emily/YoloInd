3bb39ad561da0d5da78b46fa2ede78d5961a184c
Emily Schleiner <cordial.emily@gmail.com>
2017-09-25 13:10:11 -0700

    footer formatting and style for bottom logo

--- footer.php
+++ footer.php
@@ -5,12 +5,12 @@
     <div class="bottom">
         <div class="container">
           <div class="row">
-            <div class="col-xs-4">
+            <div class="col-sm-3">
             </div>
-            <div class="col-xs-4 jumbotron">
+            <div class="col-sm-5 container-w-space">
               <?php get_template_part( 'logo'); ?>
             </div>
-            <div class="col-xs-4">
+            <div class="col-sm-4">
             </div>
           </div>
           <div class="row">
