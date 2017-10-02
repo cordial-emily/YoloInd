ce3641a6d3c762a0a7f3ea175548646fdd837d9c
Emily Schleiner <cordial.emily@gmail.com>
2017-10-02 11:43:31 -0700

    border to event list on front page and switch events and post section

--- page-front.php
+++ page-front.php
@@ -60,39 +60,40 @@
         </div>
         <div class="container container-w-space">
           <div class="row">
-            <div class="col-sm-6">
-              <h2>Our Blog</h2>
+            <div class="col-sm-6 events">
+              <h2>Upcoming Events</h2>
+              <?php echo do_shortcode("[ecs-list-events excerpt='true' eventdetails='true' thumb='true' thumbwidth='300' thumbheight='400' thumbsize='large' contentorder='thumbnail, date, title, excerpt, venue' limit='2']" ); ?>
 
-              <!--// Define our WP Query Parameters -->
-              <?php $the_query = new WP_Query( array('posts_per_page'=>5, 'category_name'=>'Front Page') ); ?>
 
-              <!--// Start our WP Query -->
-              <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-              <div class="row">
-                <div class="col-sm-3">
-                  <a href="<?php the_permalink(' ') ?>" title="<?php the_title(); ?>" class="opacity"><?php the_post_thumbnail('thumbnail', array('class' => 'thumbnail img-fluid')); ?></a>
+              </div>
+              <div class="col-sm-6">
+                <h2>Our Blog</h2>
+
+                <!--// Define our WP Query Parameters -->
+                <?php $the_query = new WP_Query( array('posts_per_page'=>5, 'category_name'=>'Front Page') ); ?>
+
+                <!--// Start our WP Query -->
+                <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+                <div class="row">
+                  <div class="col-sm-3">
+                    <a href="<?php the_permalink(' ') ?>" title="<?php the_title(); ?>" class="opacity"><?php the_post_thumbnail('thumbnail', array('class' => 'thumbnail img-fluid')); ?></a>
+                  </div>
+                  <div class="col-sm-9">
+                    <!--// Display the Post Title with Hyperlink -->
+                    <h3><a class="red" href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+
+                    <!--// Display the Post Excerpt -->
+                    <p><?php the_excerpt(__('(more…)')); ?></p>
+                  </div>
                 </div>
-                <div class="col-sm-9">
-                  <!--// Display the Post Title with Hyperlink -->
-                  <h3><a class="red" href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
-
-                  <!--// Display the Post Excerpt -->
-                  <p><?php the_excerpt(__('(more…)')); ?></p>
+                <div class="row">
+                  <div class="col-sm-12"> <hr> </div>
                 </div>
-              </div>
-              <div class="row">
-                <div class="col-sm-12"> <hr> </div>
-              </div>
-              <!--// Repeat the process and reset once it hits the limit -->
-              <?php
-              endwhile;
-              wp_reset_postdata();
-              ?>
-
-              </div>
-              <div class="col-sm-6 events">
-                <?php echo do_shortcode("[ecs-list-events excerpt='true' eventdetails='true' thumb='true' thumbwidth='300' thumbheight='150' thumbsize='large' contentorder='thumbnail, date, title, excerpt, venue']"); ?>
-
+                <!--// Repeat the process and reset once it hits the limit -->
+                <?php
+                endwhile;
+                wp_reset_postdata();
+                ?>
               </div>
             </div>
           </div>
