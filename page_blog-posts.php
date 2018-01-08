7d6bff740dbdbf56cd9748da0b2a895da0c2dd51
Emily Schleiner <cordial.emily@gmail.com>
2018-01-08 12:30:16 -0800

    individual blog post page h1 to h2

--- page_blog-posts.php
+++ page_blog-posts.php
@@ -29,7 +29,7 @@
 
 <?php while ($loop -> have_posts()): $loop -> the_post(); ?>
 <div class="jumbotron">
-  <h1><?php the_title(); ?></h1>
+  <h2><?php the_title(); ?></h2>
   <small class="text-muted"><span class="dashicons dashicons-clock"></span>&nbsp;<?php echo get_the_date( 'Y-m-d' ); ?></small>
   <hr>
   <p><?php the_content(); ?></p>
